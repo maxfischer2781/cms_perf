7c9d74abc6c80ce44889376e955f626a00998646
Max Fischer <maxfischer2781@gmail.com>
2020-07-14 17:43:02 +0200

    black

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -6,10 +6,10 @@ import pytest
 from .utility import capture
 
 
-EXECUTABLES = ['cms_perf'], [sys.executable, '-m', 'coverage', 'run', '-m', 'cms_perf']
+EXECUTABLES = ["cms_perf"], [sys.executable, "-m", "coverage", "run", "-m", "cms_perf"]
 
 
-@pytest.mark.parametrize('executable', EXECUTABLES)
+@pytest.mark.parametrize("executable", EXECUTABLES)
 def test_run_normal(executable: List[str]):
     output = capture([*executable, "--interval", "0.1"], num_lines=5)
     assert output
