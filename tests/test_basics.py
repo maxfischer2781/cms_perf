acda8efa8b1717d0bdcecf2ecdf1b2d50c727a80
Max Fischer <maxfischer2781@gmail.com>
2020-07-14 16:29:09 +0200

    configured coverage support

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,8 +1,17 @@
+from typing import List
+import sys
+
+import pytest
+
 from .utility import capture
 
 
-def test_run_normal():
-    output = capture(["cms_perf", "--interval", "0.1"], num_lines=5)
+EXECUTABLES = ['cms_perf'], [sys.executable, '-m', 'coverage', 'run', '-m', 'cms_perf']
+
+
+@pytest.mark.parametrize('executable', EXECUTABLES)
+def test_run_normal(executable: List[str]):
+    output = capture([*executable, "--interval", "0.1"], num_lines=5)
     assert output
     for line in output:
         readings = line.split()
