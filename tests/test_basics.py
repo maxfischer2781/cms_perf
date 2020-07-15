5de80b750640d39494de8bafe0e0ee8d3acedde8
Max Fischer <maxfischer2781@gmail.com>
2020-07-15 13:44:32 +0200

    Package Layout (#1)
    * turned module into package
    
    * moved metadata for flit
    
    * adjusted travis for package paths
    
    * separated CLI from sensors
    
    * moved main loop/reporting to separate module
    
    * renaming for clarity
    
    * raising on subprocess error
    
    * fixed error shadowing
    
    * console_script points to proper function
    
    * documented modules
    
    * configured coverage support
    
    * added direct tests for each sensor type
    
    * merging coverage results
    
    * tweaks for coverage, capture ignores stderr by default
    
    * added test for sched option

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,11 +1,36 @@
+from typing import List
+import sys
+
+import pytest
+
 from .utility import capture
 
 
-def test_run_normal():
-    output = capture(["cms_perf", "--interval", "0.1"], num_lines=5)
+EXECUTABLES = ["cms_perf"], [sys.executable, "-m", "coverage", "run", "-m", "cms_perf"]
+
+
+@pytest.mark.parametrize("executable", EXECUTABLES)
+def test_run_normal(executable: List[str]):
+    output = capture([*executable, "--interval", "0.1"], num_lines=5)
     assert output
     for line in output:
         readings = line.split()
         assert len(readings) == 5
         for reading in readings:
             assert 0 <= int(reading) <= 100
+
+
+@pytest.mark.parametrize("executable", EXECUTABLES)
+def test_run_sched(executable: List[str]):
+    output = capture(
+        [*executable, "--interval", "0.1", "--sched", "runq 100"],
+        num_lines=5,
+        stderr=True,
+    )
+    assert output
+    for line in output:
+        *readings, total = line.split()
+        assert len(readings) == 5
+        for reading in readings:
+            assert 0 <= int(reading) <= 100
+        assert total == readings[0]
--- tests/utility.py
+++ tests/utility.py
@@ -1,12 +1,20 @@
 from typing import List
 import itertools
 import subprocess
+import sys
+import signal
 
 
-def capture(command: List[str], num_lines=5) -> List[bytes]:
+def capture(command: List[str], num_lines=5, stderr: bool = False) -> List[bytes]:
     process = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+        command, stdout=subprocess.PIPE, stderr=(subprocess.STDOUT if stderr else None)
     )
     output = list(itertools.islice(process.stdout, num_lines))
-    process.terminate()
+    if process.poll() is not None:
+        output = b"\n".join(itertools.chain(output, process.stdout))
+        print(output, file=sys.stderr)
+        raise subprocess.CalledProcessError(
+            returncode=process.poll(), cmd=command, output=output,
+        )
+    process.send_signal(signal.SIGINT)
     return output
