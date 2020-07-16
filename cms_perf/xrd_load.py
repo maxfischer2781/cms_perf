509021fb72d879f51eb516167ffdbf116959fddc
Max Fischer <maxfischer2781@gmail.com>
2020-07-16 20:19:29 +0200

    added draft for xrootd load sensor (see #3)

---
+++ cms_perf/xrd_load.py
@@ -0,0 +1,66 @@
+"""
+P
+"""
+from typing import List
+import time
+
+import psutil
+
+
+def _rescan(interval):
+    return min(interval * 10, 60 * 60)
+
+
+def xrootd_iowait(interval):
+    tracker = XrootdTracker(rescan_interval=_rescan(interval))
+    return tracker.io_wait
+
+
+def xrootd_num_fds(interval, max_fds: int):
+    tracker = XrootdTracker(rescan_interval=_rescan(interval))
+
+    def scaled_num_fds():
+        return tracker.num_fds() / max_fds
+
+    return scaled_num_fds
+
+
+def xrootd_threads(interval, max_threads: int):
+    tracker = XrootdTracker(rescan_interval=_rescan(interval))
+
+    def scaled_num_threads():
+        return tracker.num_fds() / max_threads
+
+    return scaled_num_threads
+
+
+class XrootdTracker:
+    def __init__(self, rescan_interval: float):
+        self.rescan_interval = rescan_interval
+        self._next_scan = 0.0
+        self._xrootd_procs: List[psutil.Process] = []
+
+    @property
+    def xrootds(self) -> List[psutil.Process]:
+        if self._refresh_xrootds():
+            self._xrootd_procs = [
+                proc for proc in psutil.process_iter() if proc.name() == "xrootd"
+            ]
+            self._next_scan = time.time() + self.rescan_interval
+        return self._xrootd_procs
+
+    def _refresh_xrootds(self):
+        return (
+            not self._xrootd_procs
+            or time.time() > self.rescan_interval
+            or not all(proc.is_running() for proc in self._xrootd_procs)
+        )
+
+    def io_wait(self) -> float:
+        return max(xrd.cpu_times().iowait for xrd in self.xrootds)
+
+    def num_fds(self) -> int:
+        return sum(xrd.num_fds() for xrd in self.xrootds)
+
+    def num_threads(self) -> int:
+        return sum(xrd.num_threads() for xrd in self.xrootds)
