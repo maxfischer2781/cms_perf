871db89d5b0569307de724379c612213ebd4c19d
Max Fischer <maxfischer2781@gmail.com>
2020-07-17 12:17:23 +0200

    testing xrootd tracking

---
+++ tests/test_xrd_load.py
@@ -0,0 +1,14 @@
+import platform
+
+import pytest
+
+from cms_perf import xrd_load
+
+from .mimicry import Process
+
+
+@pytest.mark.skipif(platform.system() != "Linux", reason="Cannot mimic xrootd")
+def test_tracker():
+    tracker = xrd_load.XrootdTracker(1)
+    with Process('xrootd', threads=20, files=20):
+        assert tracker.num_threads() > 20
