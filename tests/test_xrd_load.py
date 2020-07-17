9bef8430f235aa787ffc4c18b57294c92b42f75c
Max Fischer <maxfischer2781@gmail.com>
2020-07-17 15:59:58 +0200

    basic test for io_wait functionality

--- tests/test_xrd_load.py
+++ tests/test_xrd_load.py
@@ -9,7 +9,8 @@ from .mimicry import Process
 
 @pytest.mark.skipif(platform.system() != "Linux", reason="Cannot mimic xrootd")
 def test_tracker():
-    tracker = xrd_load.XrootdTracker(1)
+    tracker = xrd_load.XrootdTracker(rescan_interval=1)
     with Process("xrootd", threads=20, files=20):
         assert tracker.num_threads() >= 20
         assert tracker.num_fds() >= 20
+        assert type(tracker.io_wait()) is float
