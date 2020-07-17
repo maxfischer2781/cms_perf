523c4c35993ad067ac201120997cadb335d8d81a
Max Fischer <maxfischer2781@gmail.com>
2020-07-17 18:37:35 +0200

    testing xrootd_load pag plugin CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,7 @@ import sys
 import pytest
 
 from .utility import capture
+from . import mimicry
 
 
 EXECUTABLES = ["cms_perf"], [sys.executable, "-m", "coverage", "run", "-m", "cms_perf"]
@@ -34,3 +35,25 @@ def test_run_sched(executable: List[str]):
         for reading in readings:
             assert 0 <= int(reading) <= 100
         assert total == readings[0]
+
+
+PAG_PLUGINS = ["xrootd.io_wait", "xrootd.num_fds", "xrootd.num_threads"]
+
+
+@mimicry.skipif_unsuported
+@pytest.mark.parametrize("executable", EXECUTABLES)
+@pytest.mark.parametrize("pag_plugin", PAG_PLUGINS)
+def test_run_pag_plugin(executable: List[str], pag_plugin):
+    with mimicry.Process(name="xrootd", threads=20, files=20):
+        output = capture(
+            [*executable, "--interval", "0.1", "--sched", "runq 100", f"pag={pag_plugin}"],
+            num_lines=5,
+            stderr=True,
+        )
+        assert output
+        for line in output:
+            *readings, total = line.split()
+            assert len(readings) == 5
+            for reading in readings:
+                assert 0 <= int(reading) <= 100
+            assert total == readings[0]
