00ad9d0f423e9b8aec2b1db80a8c7e1e733b390a
Max Fischer <maxfischer2781@gmail.com>
2020-08-04 16:25:38 +0200

    testing bare sensor factories

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -1,19 +1,17 @@
-from functools import partial
-
 import pytest
 
 from cms_perf import sensor
 
 SENSORS = (
-    partial(sensor.system_load, 0.01),
-    partial(sensor.cpu_utilization, 0.01),
+    sensor.system_load,
+    sensor.cpu_utilization,
     sensor.memory_utilization,
-    partial(sensor.network_utilization, 0.01),
+    sensor.network_utilization,
 )
 
 
 @pytest.mark.parametrize("read_sensor", SENSORS)
 def test_sensors(read_sensor):
-    result = read_sensor()
+    result = read_sensor(interval=0.01)()
     assert type(result) is float
     assert 0.0 <= result
