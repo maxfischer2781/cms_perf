b25ce5853f682f13ef9a598590beb7834f438b1a
Max Fischer <maxfischer2781@gmail.com>
2020-08-05 11:35:04 +0200

    documented CLI parser and added sample transforms

--- cms_perf/cli_parser.py
+++ cms_perf/cli_parser.py
@@ -1,3 +1,15 @@
+"""
+Parser to allow configuration, basic math and transformation on sensors via the CLI
+
+The syntax is loosely speaking:
+* float math including ``*``, ``/``, ``+``, ``-`` and parentheses
+* calls with and without arguments
+* constants such as enums
+and everything compiles down to Python source code.
+
+The math part is an explicitly defined.
+Both calls and constants are automatically generated from Python objects.
+"""
 from typing import TypeVar, Optional, Dict, NamedTuple, List, Callable, Tuple
 from typing_extensions import Protocol
 import inspect
@@ -35,18 +47,32 @@ EXPRESSION = pp.infixNotation(
 
 # Sensor Plugins
 class Sensor(Protocol):
+    """A sensor that can be registered for the CLI to provide values"""
+
     def __call__(self, *args, **kwargs) -> float:
         ...
 
 
-class SInfo(NamedTuple):
-    call: Sensor
+class Transform(Protocol):
+    """A callable that can be registered for the CLI to transform values"""
+
+    def __call__(self, *args: float) -> float:
+        ...
+
+
+S = TypeVar("S", bound=Sensor)
+CT = TypeVar("CT", bound=Transform)
+
+
+class CallInfo(NamedTuple):
+    call: Callable[..., float]
     cli_name: str
     cli_signature: List[str]
 
 
-S = TypeVar("S", bound=Sensor)
-SENSORS: Dict[str, SInfo] = {}  # transpiled_name => SFInfo
+# transpiled_name => CallInfo
+TRANSFORMS: Dict[str, CallInfo] = {}
+SENSORS: Dict[str, CallInfo] = {}
 
 
 def compile_call(
@@ -81,30 +107,48 @@ def compile_call(
     return (call_defaults,)
 
 
+# registration decorators
+# These are practically the same, but types differ and we may need to do
+# additional pre-processing for each in the future.
+def cli_transform(name: Optional[str] = None):
+    """
+    Register a transformation for the CLI with its own name or ``name``
+    """
+    assert not callable(name), "cli_transform must be called before decorating"
+
+    def register(call: CT) -> CT:
+        _register_cli_callable(call, name, TRANSFORMS)
+        return call
+
+    return register
+
+
 def cli_sensor(name: Optional[str] = None):
     """
     Register a sensor for the CLI with its own name or ``name``
     """
     assert not callable(name), "cli_sensor must be called before decorating"
 
-    def register(call: S):
-        _register_cli_sensor(call, name)
+    def register(call: S) -> S:
+        _register_cli_callable(call, name, SENSORS)
         return call
 
     return register
 
 
-def _register_cli_sensor(call: S, cli_name: Optional[str] = None) -> S:
+def _register_cli_callable(
+    call: S, cli_name: Optional[str], target: Dict[str, CallInfo]
+) -> S:
     cli_name = cli_name if cli_name is not None else call.__name__
     source_name = cli_name.replace(".", "_")
-    assert source_name not in SENSORS, f"cannot re-register sensor {source_name}"
+    assert source_name not in target, f"cannot re-register CLI callable {source_name}"
     raw_parameters = inspect.signature(call).parameters
     assert all(
         param.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD
         for param in raw_parameters.values()
-    ), "sensor factories may only take regular parameters"
+    ), "CLI callable may only take regular parameters"
     cli_parameters = [param for param in raw_parameters if param != "interval"]
-    SENSORS[source_name] = SInfo(call, cli_name, cli_parameters)
+    target[source_name] = CallInfo(call, cli_name, cli_parameters)
     GENERATED << pp.MatchFirst(
         (
             *(GENERATED.expr.exprs if GENERATED.expr else ()),
@@ -119,11 +163,12 @@ def _register_cli_sensor(call: S, cli_name: Optional[str] = None) -> S:
     return call
 
 
+# digesting of CLI information
 def prepare_sensor(
     source: str, name: Optional[str] = None
 ) -> Callable[..., Callable[[], float]]:
     name = name if name is not None else f"<cms_perf.cli_parser code {source!r}>"
-    free_variables = ", ".join(SENSORS)
+    free_variables = ", ".join(SENSORS.keys() | TRANSFORMS.keys())
     (py_source,) = EXPRESSION.parseString(source, parseAll=True)
     code = compile(
         f"lambda interval, {free_variables}: lambda: {py_source}",
@@ -137,5 +182,18 @@ def prepare_sensor(
 def compile_sensors(
     interval: float, *sensors: Callable[..., Callable[[], float]]
 ) -> List[Callable[[], float]]:
-    raw_sensors = {name: sf_info.call for name, sf_info in SENSORS.items()}
+    raw_sensors = {
+        name: sf_info.call for name, sf_info in (*SENSORS.items(), *TRANSFORMS.items())
+    }
     return [sensor(interval=interval, **raw_sensors) for sensor in sensors]
+
+
+# CLI transformations
+@cli_transform(name="max")
+def maximum(a, b):
+    return a if a >= b else b
+
+
+@cli_transform(name="min")
+def minimum(a, b):
+    return a if a <= b else b
