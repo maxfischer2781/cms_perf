c5ace411bb61cc9c83c5717dd93d371bb5ebde63
Max Fischer <maxfischer2781@gmail.com>
2024-04-24 13:59:20 +0200

    improve error message with details

--- cms_perf/setup/cli_parser.py
+++ cms_perf/setup/cli_parser.py
@@ -223,7 +223,7 @@ def _register_enum(domain: Type[enum.Enum], cli_name: Optional[str]):
     source_name = cli_name.replace(".", "_")
     assert (
         source_name not in KNOWN_DOMAINS
-    ), f"cannot re-register CLI domain {source_name}"
+    ), f"cannot re-register CLI domain {source_name} as {domain.__module__}:{domain.__qualname__}"
     cases = sorted(domain.__members__, reverse=True)
     match_case = pp.MatchFirst(tuple(map(pp.Keyword, cases))).setName(
         " | ".join(f'"{case}"' for case in cases)
